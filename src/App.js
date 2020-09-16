777ee324c6f0ce1b96d7ccb496af4859d24e8073
wongchisum <im.wongch1sum@gmail.com>
2020-09-16 18:46:19 +0800

    Commit pages and routes.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React,Hello Netlify!
-        </p>
-      </header>
-    </div>
-  );
-}
+const exactMatch = true; // 精准匹配路由
+const pathSensitive = true; // 路径大小写敏感
+
+const RouterList = [
+    { path: '/', key: '/', children: <Home /> },
+    { path: '/About', key: '/About', children: <About /> },
+    { path: '/Dashboard', key: '/Dashboard', children: <Dashboard /> },
+];
+
+const App = () => {
+    return (
+        <div className="root">
+            <Router>
+                <Switch>
+                    {RouterList.map((route) => {
+                        const { children, ...rest } = route;
+                        console.log('');
+                        return (
+                            <Route {...rest} sensitive={pathSensitive} exact={exactMatch}>
+                                {children}
+                            </Route>
+                        );
+                    })}
+                </Switch>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
